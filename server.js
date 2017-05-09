444922e2f91d5b74455f92b7f28349804f40d818
player-char <player_char@mail.ru>
2017-05-09 16:46:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,57 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'Test the bot.') {
-    try {
-        var text = JSON.stringify(message);
-    } catch(e) {
-        var text = e.name + ': ' + e.message;
+let myId = '311163859580747778';
+let ingores = [
+    myId
+];
+
+function pick(arr) {
+    return arr[Math.floor(arr.length * Math.random())];
+}
+
+function reply(message) {
+    let c = message.content;
+    let lc = c.toLowerCase().trim();
+    
+    if (lc === 'нет') {
+        return 'крипера ответ.';
     }
-    message.reply('<@' + message.author.id + '>, your message:\n```' + text + '```');
+    if (lc === 'неа') {
+        return 'крипера отвеа.';
+    }
+    if (let m = c.match(/(Dragon2488|Archengius)/)) {
+        return '`' + m[0] + '` is deprecated. Use `AntiquiAvium` instead.';
+    }
+    if (lc.match(/как так[?!]*$/)) {
+        return pick([
+            'ну вот как-то так.',
+            'как-то так, да вот как бы не так.',
+            'вот как-то так-то так вот.',
+            'как-то так, да никак-то никак.',
+            'так-то так, да как-то так как никак.'
+        ]);
+    }
+}
+
+client.on('message', message => {
+    try {
+        if (message.author.id in ignores) {
+            return;
+        }
+
+        if (message.mentions.users.has(myId)) {
+            message.react('wave');
+        }
+
+        let reply = checkReply(message);
+
+        if (reply) {
+            message.reply(reply);
+        }
+
+    } catch(e) {
+        message.reply(e.name + ': ' + e.message);
     }
 });
 
